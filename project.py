3dbf68688f3a85572f288160f8e1db3441d3d866
Sylvan <100524681+xiaownwang@users.noreply.github.com>
2024-04-01 01:54:53 -0400

    project.py

--- project.py
+++ project.py
@@ -15,6 +15,7 @@ from sklearn.svm import SVC
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import GradientBoostingClassifier
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 from warnings import simplefilter
@@ -103,8 +104,8 @@ def self_training(X_train, X_test, y_train, y_test, n_unlabelled):
     print('The level of unlabelled data is', n_unlabelled)
     # print(y_train.value_counts())  # 查看y_train数据分布
 
-    self_model = make_pipeline(CalibratedClassifierCV(LinearSVC()))  # to speed up the svc model
-    # self_model = SVC(probability=True)
+    # self_model = make_pipeline(CalibratedClassifierCV(LinearSVC()))  # to speed up the svc model
+    self_model = SVC(probability=True)
     # self_model = DecisionTreeClassifier()
     self_training_model = SelfTrainingClassifier(self_model)
     self_training_model.fit(X_train, y_train_self)
@@ -131,6 +132,7 @@ self_99 = self_training(X_train, X_test, y_train, y_test, 0.99)  #  99% unlabell
 ## Co-training Algorithm
 def co_training(X_train, X_test, y_train, y_test, num_iterations, n_unlabelled):
     t = time.time()
+    print('The level of unlabelled data is', n_unlabelled)
     X_labelled, X_unlabelled, y_labelled, y_unlabelled = train_test_split(X_train, y_train, test_size=n_unlabelled, random_state=42)
     X_view1, X_view2, y_view1, y_view2 = train_test_split(X_labelled, y_labelled, test_size=0.5, random_state=42)
 
@@ -189,13 +191,67 @@ co_99 = co_training(X_train, X_test, y_train, y_test, num_iterations=2, n_unlabe
 
 
 ## Semi-supervised Ensemble
+def semi_boosting(X_train, y_train, X_test, y_test, num_iterations, n_unlabelled):
+    t = time.time()
+    print('The level of unlabelled data is', n_unlabelled)
+    X_labelled, X_unlabelled, y_labelled, y_unlabelled = train_test_split(X_train, y_train, test_size=n_unlabelled, random_state=42)
+    y_unlabelled[0:] = -1  # -1 indicates unlabeled
 
+    weights_labelled = np.ones(len(X_labelled)) / len(X_labelled)
+    weights_unlabelled = np.ones(len(X_unlabelled)) / len(X_unlabelled)
+    weights_train = weights_labelled
 
+    for j in range(num_iterations):
+        weak_learner = DecisionTreeClassifier(max_depth=5)
+        weak_learner.fit(X_labelled, y_labelled, sample_weight=weights_train)
+        weak_learner_pred = weak_learner.predict(X_labelled)
+
+        # Update labelled weights
+        errors = np.abs(weak_learner_pred - y_labelled)
+        error_rate = np.sum(errors * weights_labelled) / np.sum(weights_labelled)
+        beta = error_rate / (1 - error_rate)
+        weights_labelled *= np.power(beta, 1 - errors)
+
+        pseudo_labels = weak_learner.predict(X_unlabelled)
+
+        # Update unlabelled weights
+        weights_unlabelled *= np.exp(-beta * pseudo_labels)
+        weights_unlabelled /= np.sum(weights_unlabelled)
+
+        # Add high confident pseudo-labeled
+        X_labelled = np.concatenate((X_labelled, X_unlabelled[pseudo_labels == y_unlabelled]))
+        y_labelled = np.concatenate((y_labelled, y_unlabelled[pseudo_labels == y_unlabelled]))
+        weights_train = np.concatenate((weights_labelled, weights_unlabelled[pseudo_labels == y_unlabelled]))
+
+        remove_unlabelled = X_unlabelled[pseudo_labels == y_unlabelled]
+        X_unlabelled = X_unlabelled.drop(index=remove_unlabelled.index, axis=0)
+        y_unlabelled = y_unlabelled.drop(index=remove_unlabelled.index, axis=0)
+        print(weights_train)
+
+    final_model = GradientBoostingClassifier(max_depth=5, n_estimators=num_iterations)
+    final_model.fit(X_labelled, y_labelled, sample_weight=weights_train)
+    y_pred = final_model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)  # accuracy
+    print("Accuracy Score of Self-training is:", acc)
+    f = f1_score(y_test, y_pred, average='micro', zero_division=1)  # f1
+    print("F1 Score of Self-training is:", f)
+    run_time = time.time() - t  # run runtime
+    print("Run time of Self-training is:", run_time,'\n')
+
+    return y_pred
+
+print('\n----------------Semi-supervised Ensemble----------------')
+boost_50 = semi_boosting(X_train, y_train, X_test, y_test, 5, 0.5)
+boost_75 = semi_boosting(X_train, y_train, X_test, y_test, 5, 0.75)
+boost_90 = semi_boosting(X_train, y_train, X_test, y_test, 5, 0.90)
+boost_99 = semi_boosting(X_train, y_train, X_test, y_test, 5, 0.99)
 
 
 
 
 ## Unsupervised Pretraining / Intrinsically Semi-supervised Learning
+
 
 
 
@@ -222,6 +278,12 @@ def plot_roc_curve(y_test, y_pred_list, labels):
 y_pred_list = []  # List to store predicted probabilities for each model
 labels = []
 # print(Counter(supervised_learning))
-y_pred_list = [supervised_learning, self_50, self_75, self_95, self_99, co_50, co_75, co_95, co_99]
-labels = ['Supervised Model', 'Self_50', 'Self_75', 'Self_95', 'Self_99', 'Co_50', 'Co_75', 'Co_95', 'Co_99']
+y_pred_list = [supervised_learning,
+               self_50, self_75, self_95, self_99,
+               co_50, co_75, co_95, co_99,
+               boost_50, boost_75, boost_90, boost_99]
+labels = ['Supervised Model',
+          'Self_50', 'Self_75', 'Self_95', 'Self_99',
+          'Co_50', 'Co_75', 'Co_95', 'Co_99',
+          'Boost_50', 'Boost_75', 'Boost_90', 'Boost_99']
 plot_roc_curve(y_test, y_pred_list, labels)
